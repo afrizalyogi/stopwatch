2b218924911c2d61638370e215be89cfc1ed9df6
Afrizal Yogi P <xxgdev@gmail.com>
2021-07-04 01:08:10 +0700

    Update assets source

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./solid.min.css";
-import "./fontawesome.min.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
